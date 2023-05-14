464029a2954d4097e4e5205f73bcf5f41f5a2bda
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-13 22:46:55 -0400

    banner style

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 function Banner() {
   return (
-    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+    <div style={{ height: '600px' }}className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10 banner-container">
       <div>
-        <h1 className="text-5xl">Perfect Harmony Trees and Gardens</h1>
+        <h1 className="text-5xl text-white">Perfect Harmony Trees and Gardens</h1>
         <h2 className="mt-5 md:mt-0">
-        <span className="underline decoration-2 decoration-[#e34747]">{" "}
-          
-        Tree trimming, tree removal, garden and playground installations
-          </span>{" "}
+          <span className="underline decoration-2 decoration-[#2c5f1f] text-white">
+            Tree trimming, tree removal, garden and playground installations
+          </span>
         </h2>
       </div>
 
@@ -20,3 +18,7 @@ function Banner() {
 }
 
 export default Banner;
+
+<style>
+
+</style>
