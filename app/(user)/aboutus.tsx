4bf29f08c877b42adf5a1ab8e7c6cf3851ae8752
jgwentworth92 <alphabetboi92@protonmail.com>
2023-05-19 22:25:29 -0400

    about page working

--- app/(user)/aboutus.tsx
+++ app/(user)/aboutus.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import Menu from '../components/Menu';
-import Footer from '../components/Footer';
-
-const AboutPage = () => {
+import React from "react";
+import Menu from "../components/Menu";
+import Footer from "../components/Footer";
+export default async function AboutPage() {
   return (
     <div>
-      <Menu />
-      
       {/* Add your "About Us" content here */}
       <h1>About Us</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-      <Footer />
     </div>
   );
-};
-
-export default AboutPage;
+}
---
+++ app/(user)/aboutus/page.tsx
@@ -0,0 +1,12 @@
+export default async function AboutPage() {
+    return (
+      <div>
+        {/* Add your "About Us" content here */}
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  
+        
+      </div>
+    );
+  }
+  
