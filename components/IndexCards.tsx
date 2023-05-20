20e92be6c7e1fcf5bec6b8cce21787bae34b975e
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-20 15:44:22 -0400

    footer and cards on index

--- components/IndexCards.tsx
+++ components/IndexCards.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ imageUrl, header, description, ctaText }) => {
   return (
     <div className="flex flex-col items-center justify-center  mb-4 pt-12">
-      <div className="mb-4">
+      <div className="mb-12">
         <Image src={imageUrl} width={200} height={200} alt="Card Image" />
       </div>
       <h2 className="text-xl font-bold mb-2">{header}</h2>
@@ -38,7 +38,7 @@ const IndexCards: React.FC = () => {
         ctaText="Action 2"
       />
       <Card
-        imageUrl="https://cdn.discordapp.com/attachments/1095866309792772239/1109325594837127188/dianadetroit_logo_of_firehose_pointing_to_the_right_bottom_corn_033a52bc-907c-4bec-a6fc-bde8a26852b8.png"
+        imageUrl="https://cdn.discordapp.com/attachments/1103865788944875622/1109565141370020022/168000900_4032798546782681_8241560606052318838_n.jpg"
         header="Card 3"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         ctaText="Action 3"
