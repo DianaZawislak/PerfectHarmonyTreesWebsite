ee4896f258269ee275c6105cc740bd850e8ca0ee
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-25 23:14:45 -0400

    styles

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -15,7 +15,7 @@ function Banner() {
     <div 
       style={{
         height: '400px',
-        backgroundImage: `url(https://cdn.discordapp.com/attachments/1103865788944875622/1110748117042724905/herobackopacity.png)`,
+        backgroundImage: `url(https://cdn.discordapp.com/attachments/1110785495157461083/1111470323498496180/herobackopacity1.png)`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         opacity: 1
@@ -30,7 +30,7 @@ function Banner() {
           <h2 className="text-white pt-5 text-lg tracking-widest">
             Tree Trimming | Tree Removal | Garden and Playground Installations | And More!!!
           </h2>
-          <button className="mt-10 px-4 py-2 bg-lime-300 text-gray-500  hover:text-lime-300 hover:bg-gray-500 rounded-lg text-xl">Call to action</button>
+          <button className="mt-10 px-4 py-2 bg-lime-300 text-gray-500  hover:text-lime-300 hover:bg-gray-500 transition-all duration-700 rounded-lg text-xl">Call to action</button>
           </div>
         </div>
       </div>
