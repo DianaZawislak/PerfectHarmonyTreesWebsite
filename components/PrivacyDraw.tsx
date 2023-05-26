c908eb6ab5d05aee934777201ea524f7d4b2caee
jgwentworth92 <alphabetboi92@protonmail.com>
2023-05-26 13:59:37 -0400

    created cookie banner and wip privacy page

--- components/PrivacyDraw.tsx
+++ components/PrivacyDraw.tsx
@@ -1,78 +1,77 @@
 "use client";
-
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
-export default function PrivacyDraw() {
-  const [open, setOpen] = useState(true);
+export default function CookieBanner() {
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const userResponse = localStorage.getItem('gdprResponse')
+    if (!userResponse) {
+      setOpen(true)
+    }
+  }, [])
+
+  function onClose() {
+    localStorage.setItem('gdprResponse', 'accepted')
+    setOpen(false)
+  }
 
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 overflow-hidden"
-        onClose={setOpen}
+        static
+        className="fixed inset-0 overflow-y-auto  z-50"
+        onClose={() => {}}
       >
         <div className="absolute inset-0 overflow-hidden">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-in-out duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in-out duration-500"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="absolute inset-0 bg-gray-500 bg-opacity-10 transition-opacity" />
-          </Transition.Child>
-
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="fixed inset-y-0 left-0 pr-10 max-w-full flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="w-screen max-w-md">
-                  <div className="h-full divide-y divide-gray-200 flex flex-col bg-white shadow-xl">
-                    <div className="px-4 py-6 sm:px-6">
-                      <div className="flex items-start justify-between space-x-3">
-                        <Dialog.Title className="text-lg font-medium text-gray-900">
-                          Panel title
-                        </Dialog.Title>
-                        <div className="h-7 flex items-center">
-                          <button
-                            type="button"
-                            className="bg-gray-100 rounded-md text-gray-400 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            onClick={() => setOpen(false)}
-                          >
-                            <span className="sr-only">Close panel</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                          </button>
-                        </div>
+          <div className="absolute inset-0 bg-gray-500 bg-opacity-10 transition-opacity" />
+          <div className="fixed inset-x-0 bottom-0 pb-4">
+            <Transition.Child
+              as={Fragment}
+              enter="transform transition ease-in-out duration-300"
+              enterFrom="translate-y-full"
+              enterTo="translate-y-0"
+              leave="transform transition ease-in-out duration-300"
+              leaveFrom="translate-y-0"
+              leaveTo="translate-y-full"
+            >
+              <Dialog.Panel className="w-full max-w-md mx-auto rounded-lg shadow-lg overflow-hidden transform transition-all sm:max-w-xl">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 sm:mt-0 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900"
+                      >
+                        Cookies Policy
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          We use cookies to improve your browsing experience.
+                          For more information, please visit our{" "}
+                          <Link
+                            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                            href="/privacy"
+                          > Privacy Policy.</Link>{" "}
+                         
+                        </p>
                       </div>
-                    </div>
-                    <div className="relative flex-1 py-6 px-4 sm:px-6">
-                      {/* Replace with your content */}
-                      <div className="absolute inset-0 py-6 px-4 sm:px-6">
-                        <div
-                          className="h-full border-2 border-dashed border-gray-200"
-                          aria-hidden="true"
-                        >
-                          Hello, we are tracking you.
-                        </div>
-                      </div>
-                      {/* /End replace */}
                     </div>
                   </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => onClose()}
+                  >
+                    Accept
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
       </Dialog>
