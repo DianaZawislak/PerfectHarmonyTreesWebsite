9ff1d6aec3fb823eeeab3351b1cc83f22718fcf2
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-29 15:40:59 -0400

    minor cookie style

--- components/PrivacyDraw.tsx
+++ components/PrivacyDraw.tsx
@@ -64,7 +64,7 @@ export default function CookieBanner() {
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
                     type="button"
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-lime-300 text-base font-medium text-gray-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => onClose()}
                   >
                     Accept
