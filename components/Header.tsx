7cb4faf43846318f312bcc2ceceaee2477f84e65
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2023-06-02 19:45:37 -0400

    dropdown is working now

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ function Header({ menu }: HeaderProps) {
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
-  console.log(menu.logoUrl);
+
   return (
     <div
       style={{
@@ -34,13 +34,13 @@ function Header({ menu }: HeaderProps) {
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
-      className="flex items-center justify-between space-x-2 font-bold px-5"
+      className="flex items-center justify-between px-5 space-x-2 font-bold"
     >
-      <div className="hidden md:flex items-center space-x-32 pr-10 justify-between w-full">
+      <div className="items-center justify-between hidden w-full pr-10 space-x-32 md:flex">
         <div className="flex items-center space-x-10">
           <Link href="/">
             <Image
-              src={menu.logoUrl}
+              src="https://cdn.discordapp.com/attachments/1103865788944875622/1108960263820030022/logonobrgdyellowgreen.png"
               width={100}
               className="rounded-md"
               height={100}
@@ -49,7 +49,7 @@ function Header({ menu }: HeaderProps) {
             />
           </Link>
           <nav className="font-semibold text-md" style={{ color: "#ECF87F" }}>
-            <ul className="flex flex-row space-x-8 text-lg  ">
+            <ul className="flex flex-row space-x-8 text-lg ">
               {menu.items.map((item: MenuItem, index: any, array: any) => (
                 <li
                   key={item?._key}
@@ -63,26 +63,28 @@ function Header({ menu }: HeaderProps) {
             </ul>
           </nav>
         </div>
-        <div className="pl-8 flex items-center">
+        <div className="flex items-center pl-8">
           <FontAwesomeIcon
-            className="pr-5 pl-12 text-2xl"
+            className="pl-12 pr-5 text-2xl"
             icon={faPhone}
             style={{ color: "#ecf87f" }}
           />
           <a
             href="tel:123-456-7890"
-            className="text-lime-300 text-xl hover:text-gray-500"
+            className="text-xl text-lime-300 hover:text-gray-500"
           >
             123-456-7890
           </a>
         </div>
       </div>
 
-      <div className="md:hidden flex justify-between w-full items-center">
+      <div className="flex items-center justify-between w-full md:hidden">
         <Link href="/">
-          <img
+          <Image
             src={menu.logoUrl}
-            className="rounded-md w-20 h-20 object-contain"
+            width={50}
+            className="rounded-md"
+            height={50}
             alt="logo"
           />
         </Link>
@@ -95,53 +97,45 @@ function Header({ menu }: HeaderProps) {
           <a
             href="tel:123-456-7890"
             style={{ color: "#ECF87F" }}
-            className="text-lime-300 pr-5 hover:text-amber-800"
+            className="pr-5 text-lime-300 hover:text-amber-800"
           >
             123-456-7890
           </a>
-       
+
+          <DropdownMenuPrimitive.Root>
+            <DropdownMenuPrimitive.Trigger asChild>
+              <button
+                className="text-2xl text-lime-300 hover:text-gray-500"
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+              >
+                <HamburgerMenuIcon
+                  className="w-6 h-6 hover:text-amber-800"
+                  style={{ color: "#ECF87F" }}
+                />
+              </button>
+            </DropdownMenuPrimitive.Trigger>
+            <DropdownMenuPrimitive.Content
+              align="end"
+              sideOffset={5}
+              className="w-48 rounded-lg px-1.5 py-1 shadow-md bg-white dark:bg-gray-800 z-50"
+            >
+              <DropdownMenuPrimitive.Item
+                className={`flex cursor-default select-none items-center rounded-md px-2 py-2 text-xs outline-none `}
+              >
+                <ul className="flex flex-col justify-end space-y-2 text-2xl">
+                  {menu.items.map((item: MenuItem) => (
+                    <li key={item?._key}>
+                      <Link href={item?.link}>{item?.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </DropdownMenuPrimitive.Item>
+            </DropdownMenuPrimitive.Content>
+          </DropdownMenuPrimitive.Root>
         </div>
       </div>
-
-      <nav
-        className={`${
-          isMenuOpen ? "block" : "hidden"
-        } md:hidden font-semibold text-md`}
-      >
-        <DropdownMenuPrimitive.Root>
-          <DropdownMenuPrimitive.Trigger asChild>
-          <button
-            className="text-2xl text-lime-300  hover:text-gray-500"
-            onClick={toggleMenu}
-            aria-label="Toggle menu"
-          >
-            <HamburgerMenuIcon
-              className="w-6 h-6  hover:text-amber-800"
-              style={{ color: "#ECF87F" }}
-            />
-          </button>
-          </DropdownMenuPrimitive.Trigger>
-          <DropdownMenuPrimitive.Content
-            align="end"
-            sideOffset={5}
-            className="w-48 rounded-lg px-1.5 py-1 shadow-md bg-white dark:bg-gray-800 z-50"
-          >
-            <DropdownMenuPrimitive.Item
-              className={`flex cursor-default select-none items-center rounded-md px-2 py-2 text-xs outline-none `}
-            >
-              <ul className="flex flex-col justify-end space-y-2 text-2xl">
-                {menu.items.map((item: MenuItem) => (
-                  <li key={item?._key}>
-                    <Link href={item?.link}>{item?.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            </DropdownMenuPrimitive.Item>
-          </DropdownMenuPrimitive.Content>
-        </DropdownMenuPrimitive.Root>
-      </nav>
     </div>
   );
 }
-
 export default Header;
