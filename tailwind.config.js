98361f7b88c06889d3217a7a9ee4f18405cf0c80
Diana Zawislak <diana.zawislak@icloud.com>
2023-06-03 00:11:53 -0400

    tailwind config clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,29 +8,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        lime: {
+          300: '#ECF87F',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-}
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        lime: {
-          300: '#ECF87F',
-        },
-      },
-    },
-  },
   plugins: [require("@tailwindcss/line-clamp")],
-};
+}
