4b06a5754126041acc383a1c926963ef9c044320
Diana Zawislak <diana.zawislak@icloud.com>
2023-06-03 00:12:02 -0400

    hero style

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,9 +2,11 @@
 import { useEffect, useState } from 'react';
 import "../styles/globals.css";
 import urlFor from "../lib/urlFor";
+
 interface HeaderProps {
   hero: Hero;
 }
+
 function Banner({ hero }: HeaderProps) {
   const [animationState, setAnimationState] = useState(false);
 
@@ -18,24 +20,33 @@ function Banner({ hero }: HeaderProps) {
   return (
     <div 
       style={{
-        height: '800px',
+        height: '900px',
+        position: 'relative'
+      }}
+    >
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
         backgroundImage: `url(${urlFor(hero.backgroundImage).url()})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        opacity: 1
-      }}
-    >
+        opacity: 0.5,
+        zIndex: -1
+      }}/>
       <div className="flex flex-col items-center justify-center font-bold px-1 py-1 h-full ">
         <div className={`p-8 rounded-lg items-center justify-center text-center ${animationState ? 'animate-snake' : ''}`}>
           {hero.CTA_Main.map((text:string, index:number) => (
-            <h1 key={index} className="lg:text-5xl text-4xl" style={{ color: '#ECF87F' }}>{text}</h1>
+            <h1 key={index} className="lg:text-5xl text-4xl text-lime-300">{text}</h1>
           ))}
           <h2 className="text-white pt-5 text-lg tracking-widest">
             {hero.ctaText_Subtitle.map((text:string, index:number) => (
               <span key={index}>{text}{index !== hero.ctaText_Subtitle.length - 1 ? ' | ' : ''}</span>
             ))}
           </h2>
-          <button className="mt-10 px-4 py-2 bg-lime-300 text-gray-500  hover:text-lime-300 hover:bg-gray-500 transition-all duration-700 rounded-lg text-xl">{hero. ctaBtnTxt}</button>
+          <button className="mt-10 px-4 py-2 bg-lime-300 text-gray-500  hover:text-lime-300 hover:bg-gray-500 transition-all duration-700 rounded-lg text-xl">{hero.ctaBtnTxt}</button>
         </div>
       </div>
     </div>
