442ee1696a6f0c721f5d8d3e3b99a28a9d793a5b
Diana Zawislak <diana.zawislak@icloud.com>
2023-06-10 19:34:39 -0400

    styles

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import IndexCard from './indexCARD';
-import ServicesIndexCards from './ServicesIndexCards';
 import { useSpring, animated } from '@react-spring/web';
 import { useInView } from 'react-intersection-observer';
+import IndexCARDwithImgBack from './indexCARDwithImgBack';
 
 interface contentProps {
   content: HeroCardArray;
@@ -25,21 +25,12 @@ const Services: React.FC<contentProps> = ({content}) => {
         top: y.interpolate((value: number) => `${value * 400}px`),
       }}
     >
-<div className="services-container mb-11 grid md:grid-cols-2 lg:grid-cols-2 justify-items-center grid-cols-1 mx-auto" style={{ maxWidth: '1800px' }}>
+<div className="services-container mb-11 gap-4 px-4 grid md:grid-cols-2 lg:grid-cols-2 justify-items-center grid-cols-1 mx-auto" style={{ maxWidth: '1800px' }}>
 <div style={{ position: 'relative', maxWidth: '800px' }}> 
-    <div style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      opacity:'0.1',
-      zIndex: -1
-    }} />
+
     <IndexCard
       header="FIRST SERVICE"
+     
       backgroundImage="https://cdn.discordapp.com/attachments/1103865788944875622/1115813522497093773/dianadetroit_simple_chainsaw_outline_for_icon_black_and_white_f65c15c8-ddf1-4fc3-823d-3ebac9520d5e.png"
       cardText="This is the content of the first service.This is the content of the first service.
       This is the content of the first service.This is the content of the first service.This is the content of the first service.This is the content of the first service."
@@ -47,17 +38,7 @@ const Services: React.FC<contentProps> = ({content}) => {
     />
   </div>
   <div style={{ position: 'relative', maxWidth: '800px' }}> 
-    <div style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      opacity: '0.1',
-      zIndex: -1
-    }} />
+
     <IndexCard
       header="SECOND SERVICE"
       backgroundImage="https://cdn.discordapp.com/attachments/1103865788944875622/1115666168250912899/dianadetroit_tree_logo_black_and_white_d7deccd4-0856-4aec-80cb-c32e583920d1.png"
@@ -68,41 +49,23 @@ const Services: React.FC<contentProps> = ({content}) => {
   </div>
 </div>
 
-<div className="services-container mb-11 grid md:grid-cols-2 lg:grid-cols-3 justify-items-center grid-cols-1 mx-auto" style={{ maxWidth: '1800px' }}>
+<div className="services-container mb-11 gap-8 grid md:grid-cols-2 px-2 lg:grid-cols-3 justify-items-center grid-cols-1 mx-auto" style={{ maxWidth: '1800px' }}>
 <div style={{ position: 'relative', maxWidth: '400px' }}> 
-    <div style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      opacity:'0.1',
-      zIndex: -1
-    }} />
-    <IndexCard
-      header="FIRST SERVICE"
-      backgroundImage="https://cdn.discordapp.com/attachments/1103865788944875622/1115813522497093773/dianadetroit_simple_chainsaw_outline_for_icon_black_and_white_f65c15c8-ddf1-4fc3-823d-3ebac9520d5e.png"
-      cardText="This is the content of the first service.This is the content of the first service.
-      This is the content of the first service.This is the content of the first service.This is the content of the first service.This is the content of the first service."
+
+    <IndexCARDwithImgBack
+      header="SECOND SERVICE"
+      fullBackgroundImage='https://cdn.discordapp.com/attachments/1110785495157461083/1117234852308189325/346969329_1380028732785287_3315446062634646354_n.jpg'
+      backgroundImage="https://cdn.discordapp.com/attachments/1103865788944875622/1115666168250912899/dianadetroit_tree_logo_black_and_white_d7deccd4-0856-4aec-80cb-c32e583920d1.png"
+      cardText="This is the content of the first service.This is the content of the first service.This is the content of the first service.
+      This is the content of the first service.This is the content of the first service.This is the content of the first service"
       ctaBtnTxt="Learn More"
     />
   </div>
   <div style={{ position: 'relative', maxWidth: '400px' }}> 
-    <div style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      opacity: '0.1',
-      zIndex: -1
-    }} />
-    <IndexCard
+
+    <IndexCARDwithImgBack
       header="SECOND SERVICE"
+      fullBackgroundImage='https://cdn.discordapp.com/attachments/1110785495157461083/1117234504835289138/168000900_4032798546782681_8241560606052318838_n.jpg'
       backgroundImage="https://cdn.discordapp.com/attachments/1103865788944875622/1115666168250912899/dianadetroit_tree_logo_black_and_white_d7deccd4-0856-4aec-80cb-c32e583920d1.png"
       cardText="This is the content of the first service.This is the content of the first service.This is the content of the first service.
       This is the content of the first service.This is the content of the first service.This is the content of the first service"
@@ -111,19 +74,10 @@ const Services: React.FC<contentProps> = ({content}) => {
   </div>
 
   <div style={{ position: 'relative', maxWidth: '400px' }}> 
-    <div style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      opacity: '0.1',
-      zIndex: -1
-    }} />
-    <IndexCard
+
+    <IndexCARDwithImgBack
       header="SECOND SERVICE"
+      fullBackgroundImage='https://cdn.discordapp.com/attachments/1110785495157461083/1117234020489629816/336567321_1415091715957877_1358520114622384673_n.jpg'
       backgroundImage="https://cdn.discordapp.com/attachments/1103865788944875622/1115666168250912899/dianadetroit_tree_logo_black_and_white_d7deccd4-0856-4aec-80cb-c32e583920d1.png"
       cardText="This is the content of the first service.This is the content of the first service.This is the content of the first service.
       This is the content of the first service.This is the content of the first service.This is the content of the first service"
---
+++ components/indexCARDwithImgBack.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image";
+
+interface CardProps {
+  header: string;
+  backgroundImage: string;
+  fullBackgroundImage: string; // This will be the new prop for the full background image
+  cardText: string;
+  ctaBtnTxt: string;
+}
+
+const IndexCARDwithImgBack: React.FC<CardProps> = ({ header, backgroundImage, fullBackgroundImage, cardText, ctaBtnTxt }) => {
+  return (
+    <div className="card-container relative"> 
+      <Image // Full background image
+        src={fullBackgroundImage}
+        alt="Full Background Image"
+        layout="fill"
+        objectFit="cover"
+        className="opacity-50" // This applies opacity
+      />
+      <div
+        className="absolute inset-0 bg-gradient-to-b from-transparent to-white" // This applies gradient
+      />
+      <div className="relative flex flex-col items-center justify-center pb-10">
+        <div
+          className="mb-6 relative"
+          style={{ width: "80px", height: "80px" }}
+        >
+          <Image
+            src={backgroundImage}
+            alt="Card Image"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-md text-gray-500 font-bold mb-1">{header}</h1>
+          <p className="text-gray-500 text-md mb-4 px-15 text-center">
+            {cardText}
+          </p>
+          <button className="px-20 py-1 bg-lime-300 text-gray-500 hover:text-lime-300 hover:bg-gray-500 transition-all duration-700 font-bold text-lg rounded-lg">
+            {ctaBtnTxt}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IndexCARDwithImgBack;
